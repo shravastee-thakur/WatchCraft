ed507708c06269d3e56025917c92e59b9aa0d486
Shravastee Thakur <shravastee@gmail.com>
2024-08-17 15:58:36 +0530

    Login form added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,55 +3,97 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [profile, setProfile] = useState(false);
+  const [visible, setVisible] = useState(200);
   return (
-    <nav className="w-full bg-[#191970] px-8 flex items-center justify-between">
-      <div>
-        <h1 className="text-2xl font-bold text-white p-4">WatchCraft</h1>
-      </div>
-      <div className="px-6 flex justify-center items-center gap-8">
-        <ul className="hidden md:font-semibold md:flex md:gap-8 md:items-center">
-          <Link to={"/"}>
-            <li className="text-white hover:text-yellow-400">Home</li>
-          </Link>
-          <Link to={"/edifice"}>
-            <li className="text-white hover:text-yellow-400">Edifice</li>
-          </Link>
-          <Link to={"/enticer"}>
-            <li className="text-white hover:text-yellow-400">Enticer</li>
-          </Link>
-          <Link to={"/login"}>
-            <li className="text-white hover:text-yellow-400">Log In</li>
-          </Link>
-          <Link to={"/signup"}>
-            <li className="text-white hover:text-yellow-400">Sign Up</li>
-          </Link>
-          <li>
-            <button onClick={() => setProfile(!profile)}>
-              <i className="ri-user-fill text-white text-xl"></i>
-              {profile && (
-                <div className="absolute top-14 right-0 bg-green-300 w-[180px] p-6 shadow-lg">
-                  <h1 className="text-lg font-semibold">Shravastee</h1>
-                  <div className="border h-px my-4" />
-                  <button>
-                    <i className="ri-logout-circle-r-line mr-2"></i>
-                    Logout
-                  </button>
-                </div>
-              )}
-            </button>
-          </li>
-        </ul>
-        <button>
-          <div className="flex items-center justify-center relative">
-            <i className="ri-shopping-cart-line text-2xl text-white"></i>
-            <div className="w-5 h-5 rounded-full bg-red-600 text-white flex justify-center items-center absolute -top-2 -right-2">
-              0
+    <>
+      <nav className="w-full bg-[#191970] px-8 flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-bold text-white p-4">WatchCraft</h1>
+        </div>
+        <div className="px-6 flex justify-center items-center gap-8">
+          <ul className="hidden md:font-semibold md:flex md:gap-8 md:items-center">
+            <Link to={"/"}>
+              <li className="text-white hover:text-yellow-400">Home</li>
+            </Link>
+            <Link to={"/edifice"}>
+              <li className="text-white hover:text-yellow-400">Edifice</li>
+            </Link>
+            <Link to={"/enticer"}>
+              <li className="text-white hover:text-yellow-400">Enticer</li>
+            </Link>
+            <Link to={"/login"}>
+              <li className="text-white hover:text-yellow-400">Log In</li>
+            </Link>
+            <Link to={"/signup"}>
+              <li className="text-white hover:text-yellow-400">Sign Up</li>
+            </Link>
+            <li>
+              <button onClick={() => setProfile(!profile)}>
+                <i className="ri-user-fill text-white text-xl"></i>
+                {profile && (
+                  <div className="absolute top-14 right-0 bg-green-300 w-[180px] p-6 shadow-lg">
+                    <h1 className="text-lg font-semibold">Shravastee</h1>
+                    <div className="border h-px my-4" />
+                    <button>
+                      <i className="ri-logout-circle-r-line mr-2"></i>
+                      Logout
+                    </button>
+                  </div>
+                )}
+              </button>
+            </li>
+          </ul>
+
+          {/* shopping cart  */}
+
+          <button>
+            <div className="flex items-center justify-center relative">
+              <i className="ri-shopping-cart-line text-2xl text-white"></i>
+              <div className="w-5 h-5 rounded-full bg-red-600 text-white flex justify-center items-center absolute -top-2 -right-2">
+                0
+              </div>
+            </div>
+          </button>
+
+          {/* menu  */}
+          <button onClick={() => setVisible(visible === 200 ? 0 : 200)}>
+            <i className="ri-menu-line text-white text-xl md:hidden"></i>
+          </button>
+        </div>
+      </nav>
+      <aside>
+        <div
+          className="h-screen bg-slate-200 fixed top-0 right-0 z-30 md:hidden"
+          style={{
+            width: visible,
+            transition: "0.3s",
+          }}
+        >
+          <div className="py-4 px-4">
+            <div className="text-2xl">
+              <button onClick={() => setVisible(0)}>x</button>
             </div>
           </div>
-        </button>
-        <i className="ri-menu-line text-white text-xl md:hidden"></i>
-      </div>
-    </nav>
+          <ul className="font-semibold flex flex-col gap-8 items-center overflow-hidden md:hidden">
+            <Link to={"/"}>
+              <li className="text-black hover:text-red-400 pt-12">Home</li>
+            </Link>
+            <Link to={"/edifice"}>
+              <li className="text-black hover:text-red-400">Edifice</li>
+            </Link>
+            <Link to={"/enticer"}>
+              <li className="text-black hover:text-red-400">Enticer</li>
+            </Link>
+            <Link to={"/login"}>
+              <li className="text-black hover:text-red-400">Log In</li>
+            </Link>
+            <Link to={"/logout"}>
+              <li className="text-black hover:text-red-400">Log out</li>
+            </Link>
+          </ul>
+        </div>
+      </aside>
+    </>
   );
 };
 
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const SignUp = () => {
+  const [password, setPassword] = useState("password");
   return (
     <>
       <Navbar />
-      <div>SignUp</div>
+      <div className="w-full md:h-screen flex justify-center items-center">
+        <div className="w-4/5 p-4 md:w-1/3 bg-white md:p-8 rounded-lg ">
+          <h1 className="text-xl md:text-3xl font-semibold text-center mb-2">
+            Register
+          </h1>
+          <p className="mb-6 text-center">New users create your account</p>
+          <form className="space-y-4">
+            <div className="flex flex-col">
+              <label className="text-sm font-semibold md:text-lg">
+                Fullname
+              </label>
+              <input
+                required
+                type="text"
+                placeholder="Enter your name"
+                name="fullname"
+                className="p-2 border border-gray-300 rounded mt-1"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="text-sm font-semibold md:text-lg">Email</label>
+              <input
+                required
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                className="p-2 border border-gray-300 rounded mt-1"
+              />
+            </div>
+
+            <div className="flex flex-col relative">
+              <label className="text-sm font-semibold md:text-lg">
+                Password
+              </label>
+              <input
+                required
+                type={password}
+                placeholder="********"
+                name="password"
+                className="p-2 border border-gray-300 rounded mt-1"
+              />
+              <button
+                onClick={() =>
+                  setPassword(password === "password" ? "text" : "password")
+                }
+                type="button"
+                className="absolute top-9 right-4 w-8 h-8 rounded-full hover:bg-blue-100 hover:text-blue-600"
+              >
+                {password === "password" ? (
+                  <i class="ri-eye-line"></i>
+                ) : (
+                  <i class="ri-eye-off-line"></i>
+                )}
+              </button>
+            </div>
+
+            <div className="flex justify-center">
+              <button className="py-2 px-6 md:py-3 md:px-8 rounded bg-blue-800 text-white font-semibold hover:bg-green-800">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
       <Footer />
     </>
   );
